59ec3007b56ea8ef94c6b132cdef738bad13fca4
TonoLeo <s20c1078un@s.chibakoudai.jp>
2022-05-25 13:45:33 +0900

    Fix comment out

--- scripts/logicool_cmd_vel.py
+++ scripts/logicool_cmd_vel.py
@@ -103,10 +103,10 @@ class JoyTwist(object):
             self.level -= 1
 
 if __name__ == '__main__':
-    # rospy.wait_for_service('/motor_on')
-    # rospy.wait_for_service('/motor_off')
-    # rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
-    # rospy.ServiceProxy('/motor_on', Trigger).call()
+    rospy.wait_for_service('/motor_on')
+    rospy.wait_for_service('/motor_off')
+    rospy.on_shutdown(rospy.ServiceProxy('/motor_off', Trigger).call)
+    rospy.ServiceProxy('/motor_on', Trigger).call()
     rospy.init_node('logicool_cmd_vel')
     unko = False
     logicool_cmd_vel = JoyTwist()
